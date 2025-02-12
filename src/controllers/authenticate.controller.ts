5d194ff8523d4ba4c6ef79bd1959ae4f8ac97ca3
NaN <ipfilus@gmail.com>
2025-02-11 23:01:29 -0300

    feat: add authentication controller

---
+++ src/controllers/authenticate.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, HttpCode, Post, UnauthorizedException, UsePipes } from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { JwtService } from "@nestjs/jwt";
+import { compare } from "bcryptjs";
+import { PrismaService } from "src/prisma/prisma.service";
+const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+});
+
+type AuthenticateBody = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/sessions')
+export class AuthenticateController {
+    constructor(private jwt: JwtService, private prisma: PrismaService) {}
+
+    @Post()
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+    async handle(@Body() body: AuthenticateBody) {
+        const { email, password } = body;
+
+        const user = await this.prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const isPasswordValid = await compare(password, user.password);
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const accessToken = this.jwt.sign({ sub: user.id })
+
+        return {
+            access_token: accessToken,
+        }
+
+    }
+}
+
