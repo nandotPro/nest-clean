162989d1ec8ce1f0ebee9830870428f840ee3c6e
NaN <ipfilus@gmail.com>
2025-02-11 22:09:21 -0300

    feat: add jwt config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { envSchema } from './env';
-
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    validate: (env) => envSchema.parse(env),
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
