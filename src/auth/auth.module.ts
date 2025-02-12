eca6ac70f96fc955bac623fac1142c54fcae4725
NaN <ipfilus@gmail.com>
2025-02-12 17:46:41 -0300

    feat: add guard protection

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import { Env } from "src/env";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
     imports: [
@@ -20,6 +21,7 @@ import { Env } from "src/env";
                 }
             },
         })
-    ]
+    ],
+    providers: [JwtStrategy],
 })
 export class AuthModule {}
---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, HttpCode, Post, UnauthorizedException, UsePipes, UseGuards } from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
+import { JwtService } from "@nestjs/jwt";
+import { compare } from "bcryptjs";
+import { PrismaService } from "src/prisma/prisma.service";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+
+const createQuestionBodySchema = z.object({
+    title: z.string(),
+    content: z.string(),
+});
+
+type CreateQuestionBody = z.infer<typeof createQuestionBodySchema>;
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+    constructor(private jwt: JwtService, private prisma: PrismaService) {}
+
+    @Post()
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
+    async handle(@Body() body: CreateQuestionBody) {
+        
+
+    }
+}
+
