c874246ba58a49f4ba30d568c86cc0b9b9667341
NaN <ipfilus@gmail.com>
2025-02-11 21:53:20 -0300

    feat: add configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    validate: (env) => envSchema.parse(env),
+  }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
