92062991470edf9d4d2c724795fa0412505b1f4d
NaN <ipfilus@gmail.com>
2025-02-13 20:47:33 -0300

    feat: add role and answers to prisma schema

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -9,7 +9,7 @@ export class PrismaQuestionMapper {
             title: raw.title,
             content: raw.content,
             authorId: new UniqueEntityID(raw.authorId),
-            bestAnswerId: undefined,
+            bestAnswerId: raw.bestAnswerId ? new UniqueEntityID(raw.bestAnswerId) : null,
             slug: Slug.create(raw.title),
             createdAt: raw.createdAt,
             updatedAt: raw.updatedAt,
