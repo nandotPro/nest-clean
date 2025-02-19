af5c46dee9483bd14676ea7adec4ab46d705e851
NaN <ipfilus@gmail.com>
2025-02-18 23:05:40 -0300

    feat: add edit-answer controller and e2e test

--- src/domain/forum/application/use-cases/edit-answer.ts
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -7,6 +7,7 @@ import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { AnswerAttachment } from "../../enterprise/entities/answer-attachment";
 import { AnswerAttachmentList } from "../../enterprise/entities/answer-attachment-list";
 import { AnswerAttachmentsRepository } from "../repositories/answer-attachments-repository";
+import { Injectable } from "@nestjs/common";
 
 interface EditAnswerRequest {
     answerId: string;
@@ -22,6 +23,7 @@ type EditAnswerResponse = Either<
     }
 >
 
+@Injectable()
 export class EditAnswerUseCase {
     constructor(
         private answersRepository: AnswersRepository,
---
+++ src/infra/http/controllers/edit-answer.controller.ts
@@ -0,0 +1,54 @@
+import { BadRequestException, Body, Controller, ForbiddenException, HttpCode, NotFoundException, Param, Put, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "@/infra/auth/current-user-decorator";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { UserPayload } from "@/infra/auth/jwt.strategy";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { EditAnswerUseCase } from "@/domain/forum/application/use-cases/edit-answer";
+import { ResourceNotFoundError } from "@/core/errors/errors/resouce-not-found-error";
+import { NotAllowedError } from "@/core/errors/errors/not-allowed-error";
+
+const editAnswerBodySchema = z.object({
+    content: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(editAnswerBodySchema);
+
+type EditAnswerBodySchema = z.infer<typeof editAnswerBodySchema>;
+
+@Controller('/answers/:id')
+@UseGuards(JwtAuthGuard)
+export class EditAnswerController {
+    constructor(private editAnswer: EditAnswerUseCase) {}
+
+    @Put()
+    @HttpCode(204)
+    async handle(
+        @Body(bodyValidationPipe) body: EditAnswerBodySchema,
+        @CurrentUser() user: UserPayload,
+        @Param('id') answerId: string,
+    ) {
+        const { content } = body;
+        const userId = user.sub;
+
+        const result = await this.editAnswer.execute({
+            answerId,
+            authorId: userId,
+            content,
+            attachmentsIds: [],
+        });
+
+        if (result.isLeft()) {
+            const error = result.value;
+
+            switch (error.constructor) {
+                case ResourceNotFoundError:
+                    throw new NotFoundException();
+                case NotAllowedError:
+                    throw new ForbiddenException(error.message);
+                default:
+                    throw new BadRequestException();
+            }
+        }
+    }
+} 
