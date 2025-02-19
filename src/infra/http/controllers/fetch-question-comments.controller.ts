9e9a718a36d76324e1cd89aed6afbb0681116ce3
NaN <ipfilus@gmail.com>
2025-02-19 16:35:42 -0300

    feat: add fetch-question-comments controller and e2e test

---
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -0,0 +1,39 @@
+import { BadRequestException, Controller, Get, Param, Query, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard";
+import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
+import { z } from "zod";
+import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-question-comments";
+import { CommentPresenter } from "../presenters/comment-presenter";
+
+const pageQueryParamSchema = z.object({
+    page: z.string().optional().default('1').transform(Number),
+});
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+@Controller('/questions/:questionId/comments')
+@UseGuards(JwtAuthGuard)
+export class FetchQuestionCommentsController {
+    constructor(private fetchQuestionComments: FetchQuestionCommentsUseCase) {}
+
+    @Get()
+    async handle(
+        @Query(queryValidationPipe) query: PageQueryParamSchema,
+        @Param('questionId') questionId: string,
+    ) {
+        const result = await this.fetchQuestionComments.execute({
+            page: query.page,
+            questionId,
+        });
+
+        if (result.isLeft()) {
+            throw new BadRequestException();
+        }
+
+        const comments = result.value.comments;
+
+        return { comments: comments.map(CommentPresenter.toHTTP) };
+    }
+} 
